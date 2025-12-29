2fa11454879adf2048fd59377eb83791a8cd99f2
harishfoviz-afk <harish.foviz@gmail.com>
2025-12-29 16:20:33 +0530

    Update create-order.js

--- netlify/functions/create-order.js
+++ netlify/functions/create-order.js
@@ -1,7 +1,7 @@
 // netlify/functions/create-order.js
 const Razorpay = require('razorpay');
 
-export const handler = async (event) => {
+exports.handler = async (event) => {
     const rzp = new Razorpay({
         key_id: process.env.RAZORPAY_KEY_ID,
         key_secret: process.env.RAZORPAY_KEY_SECRET,
@@ -10,13 +10,14 @@ export const handler = async (event) => {
     const { amount, internalOrderId } = JSON.parse(event.body);
 
     const options = {
-        amount: amount, // Amount in paise
+        amount: amount, 
         currency: "INR",
-        receipt: internalOrderId,
+        receipt: internalOrderId, // This is your AS1-XXX ID
     };
 
     try {
         const order = await rzp.orders.create(options);
+        // This 'order' object contains the 'id' (order_XXXXX) needed for verification
         return { statusCode: 200, body: JSON.stringify(order) };
     } catch (error) {
         return { statusCode: 500, body: JSON.stringify(error) };
